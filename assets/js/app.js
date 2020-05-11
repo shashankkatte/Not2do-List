25e5efcae0c58d4cdfb2e08724a316602a64734e
shashankkatte <shashankanand.katte@gmail.com>
2020-05-11 12:10:54 -0400

    Added list item fadeout and removal

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,11 @@
 $('li').click(function () {
   $(this).toggleClass('completed');
 });
+
+//  Click on X to delete todo
+$('span').click(function (event) {
+  $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+  });
+  event.stopPropagation();
+});
