d7614292845c3914f2127be49b0d5250c2ab7d55
shashankkatte <shashankanand.katte@gmail.com>
2020-05-11 14:55:43 -0400

    STYLING : Updated CSS Styling and refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,26 @@
-//  check off specific Not 2 Dos by clicking
-$('ul').on('click', 'li', function () {
-  $(this).toggleClass('completed');
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
 });
 
-//  Click on X to delete todo
-$('ul').on('click', 'span', function (event) {
-  $(this)
-    .parent()
-    .fadeOut(500, function () {
-      $(this).remove();
-    });
-  event.stopPropagation();
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
 
-//  adding a new not2do list item
-$("input[type='text']").keypress(function (event) {
-  //  check if enter key
-  if (event.which === 13) {
-    //  grabbing new not2do item from input text
-    let todoText = $(this).val();
-    $(this).val('');
-    // Create new li and add to ul
-    $('ul').append(`<li><span>X </span>${todoText}</li>`);
-  }
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
 });
