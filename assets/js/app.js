039a7270fb2c00a9bcc9e7c25eaa18969c9b3880
shashankkatte <shashankanand.katte@gmail.com>
2020-05-11 12:42:42 -0400

    Basic list items add and delete implimented

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,26 @@
 //  check off specific Not 2 Dos by clicking
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
   $(this).toggleClass('completed');
 });
 
 //  Click on X to delete todo
-$('span').click(function (event) {
-  $(this).parent().fadeOut(500, function(){
+$('ul').on('click', 'span', function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
       $(this).remove();
-  });
+    });
   event.stopPropagation();
+});
+
+//  adding a new not2do list item
+$("input[type='text']").keypress(function (event) {
+  //  check if enter key
+  if (event.which === 13) {
+    //  grabbing new not2do item from input text
+    let todoText = $(this).val();
+    $(this).val('');
+    // Create new li and add to ul
+    $('ul').append(`<li><span>X </span>${todoText}</li>`);
+  }
 });
